a96ea70f245c87fd7e602c181fcd80c188a48f2b
Paulo Alves <juninhpaulo@hotmail.com>
2018-04-27 03:19:34 -0300

    Translate API (yandex) check

--- App.js
+++ App.js
@@ -1,13 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      untranslated: 'Linear algebra is a branch of mathematics that is widely used throughout science and engineering.',
+      translated: '',
+      translatedConfirm: '',
+    }
+  }
+
+  send() {
+    let body = {
+      key: '',      
+      format: 'plain',
+      lang: 'en-pt',
+      text: 'Linear algebra is a branch of mathematics that is widely used throughout science and engineering.',
+    }
+
+    let formBody = [];
+    for (let key in body) { formBody.push(encodeURIComponent(key) + "=" + encodeURIComponent(body[key])); }
+    formBody = formBody.join("&");
+
+    fetch('https://translate.yandex.net/api/v1.5/tr.json/translate', {
+      method: 'post',
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: formBody
+    })
+    .then(response => response.json())
+    .then(translated => {
+      //console.warn(translated.text[0])
+      this.setState({...this.state, translatedConfirm: translated.text[0]})
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View 
+        style={styles.container}>
+        <Text
+          style={styles.untranslated}
+        >
+          {this.state.untranslated}
+        </Text>
+
+        <Text>
+          {this.state.translatedConfirm}
+        </Text>
+
+        <TextInput
+          style={styles.translated}
+          placeholder="Traduza o texto acima!"
+          onChangeText={translated => this.setState({ translated })}
+        />
+
+        <Button
+          onPress={this.send.bind(this)}
+          title="Send"          
+          accessibilityLabel="Send"
+        />
       </View>
     );
   }
@@ -20,4 +74,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  sendButton: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+
+  untranslated: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20
+  },
+
+  translated: {
+    marginTop: 20,
+  }
 });
